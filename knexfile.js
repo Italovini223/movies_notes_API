8f31adc952a31c8e0b76960a426df781f97c8cb6
italo <italovinicius2018@gmail.com>
2022-08-17 01:03:59 -0300

    add a migrations folder

--- knexfile.js
+++ knexfile.js
@@ -6,7 +6,9 @@ module.exports = {
     connection: {
       filename: path.resolve(__dirname, "src", "database", "database.db"),
     },
-
+    migrations: {
+      
+    }
     useNullAsDefault: true
   },
 
