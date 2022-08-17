99ee78b5c3f277327db4ce29da354867d13a882b
italo <italovinicius2018@gmail.com>
2022-08-16 23:51:14 -0300

    create a database

--- src/server.js
+++ src/server.js
@@ -1,3 +1,4 @@
+const database = require("./database/sqlite");
 const express = require('express');
 const app = express();
 
@@ -8,5 +9,7 @@ const PORT = 3333;
 
 app.use(express.json())
 app.use(routes)
+
+database();
 
 app.listen(PORT, () => {console.log(`Server is running on port ${PORT}`);});
