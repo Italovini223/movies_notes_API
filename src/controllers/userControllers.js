aea47a3fd8190e09941bed736a4969b326e56e07
italo <italovinicius2018@gmail.com>
2022-08-17 02:14:51 -0300

    registering user in the database

--- src/controllers/userControllers.js
+++ src/controllers/userControllers.js
@@ -1,13 +1,30 @@
-const knex = require("../database/knex")
+const knex = require("../database/knex");
+const {hash} = require("bcryptjs");
+const sqliteConnection = require("../database/sqlite");
+const appError = require("../utils/appError");
+
 class userControllers {
  async create(request, response) {
     const {name, email, password} = request.body;
 
+    const database = await sqliteConnection();
+    const checkUserExists = await database.get("SELECT * FROM users WHERE email = (?)", [email]);
+    
+    if(checkUserExists) {
+      throw new appError("User already exists")
+    }
+    
+    const hashedPassword = await hash(password, 8);
+
     await knex("users").insert({
       name,
       email,
-      password
-    })
+      password: hashedPassword
+    });
+
+    return response.status(200).json({
+      message: "users registered successfully"
+    });
 
   }
 }
