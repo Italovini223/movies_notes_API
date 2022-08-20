9a029e943b348e694cec2fae015a81a009bad30b
italo <italovinicius2018@gmail.com>
2022-08-20 13:40:11 -0300

    ✨feat: import the movie routes

---
+++ src/routes/movieNotes.routes.js
@@ -0,0 +1,5 @@
+const {Router} = require("express");
+const movieNotesControllers = require("../controllers/movieNotesControllers");
+
+const notesRouter = Router();
+const notesController = new movieNotesControllers();
