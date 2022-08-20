f24e1b2d7ac30974c203ac5af620c1688b9ddd8e
italo <italovinicius2018@gmail.com>
2022-08-20 12:38:45 -0300

    ✨feat: add a const to receive the req form body

--- src/controllers/movieNotesControllers.js
+++ src/controllers/movieNotesControllers.js
@@ -2,6 +2,9 @@ const knex = require("../database/knex");
 const appError = require("../utils/appError");
 class movieNotesControllers {
   async create(request, response){
+    const {title, description, rating, tags} = request.body;
+
+
   }
 }
 
