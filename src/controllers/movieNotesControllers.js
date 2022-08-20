cd059addaf4af0dc6e5ddb52d73abc0a63a57946
italo <italovinicius2018@gmail.com>
2022-08-20 12:29:44 -0300

    ✨feat: add a function to create notes

---
+++ src/controllers/movieNotesControllers.js
@@ -0,0 +1,8 @@
+const knex = require("../database/knex");
+const appError = require("../utils/appError");
+class movieNotesControllers {
+  async create(request, response){
+  }
+}
+
+module.exports = MovieNotesController;
