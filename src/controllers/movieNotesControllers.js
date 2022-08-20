817c2f84a4178d5637a02f78eb87410c569429b4
italo <italovinicius2018@gmail.com>
2022-08-20 13:15:29 -0300

    ✨feat: inset the note data in database

--- src/controllers/movieNotesControllers.js
+++ src/controllers/movieNotesControllers.js
@@ -4,6 +4,13 @@ class movieNotesControllers {
   async create(request, response){
     const {title, description, rating, tags} = request.body;
     const {user_id} = request.params;
+
+    const note_id = await knex("movie_notes").insert({
+      title,
+      description,
+      rating
+    });
+
   }
 }
 
