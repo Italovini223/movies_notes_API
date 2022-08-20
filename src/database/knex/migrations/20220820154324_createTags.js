032e43ed2f0d69c4f715677e6aba6d124bbe073d
italo <italovinicius2018@gmail.com>
2022-08-20 13:05:39 -0300

    ✨feat: add the columns on tags table

--- src/database/knex/migrations/20220820154324_createTags.js
+++ src/database/knex/migrations/20220820154324_createTags.js
@@ -1,15 +1,12 @@
-/**
- * @param { import("knex").Knex } knex
- * @returns { Promise<void> }
- */
-exports.up = function(knex) {
-  
-};
+exports.up = knex => knex.schema.createTable("tags", table => {
+  table.increments("id");
 
-/**
- * @param { import("knex").Knex } knex
- * @returns { Promise<void> }
- */
-exports.down = function(knex) {
-  
-};
+  table.integer("note_id").references("id").inTable("movie_notes").onDelete("CASCADE");
+  table.integer("user_id").references("id").inTable("users").onDelete("CASCADE");
+
+  table.text("name");
+
+});
+
+
+exports.down = knex => knex.schema.dropTable("tags");
