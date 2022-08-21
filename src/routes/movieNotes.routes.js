32c7927ae378e01d5e296343c192c302409bb5c8
italo <italovinicius2018@gmail.com>
2022-08-21 11:50:16 -0300

    ✨feat: add a route to show the notes

--- src/routes/movieNotes.routes.js
+++ src/routes/movieNotes.routes.js
@@ -5,6 +5,7 @@ const notesRouter = Router();
 const notesController = new movieNotesControllers();
 
 notesRouter.post("/:user_id", notesController.create);
+notesRouter.get("/:id", notesController.show);
 
 module.exports = notesRouter;
 
