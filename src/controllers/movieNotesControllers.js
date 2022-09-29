8bdb18119f4d85ef9dee38e2945b880623bea304
Italovini223 <italovinicius2018@gmail.com>
2022-09-28 23:56:21 -0300

    test git

--- src/controllers/movieNotesControllers.js
+++ src/controllers/movieNotesControllers.js
@@ -2,6 +2,7 @@ const knex = require("../database/knex");
 const appError = require("../utils/appError");
 
 class movieNotesControllers {
+  
   async create(request, response){
     const {title, description, rating, tags} = request.body;
     const {user_id} = request.params;
