d8888893a0942e2c772f63af1500322581f7e673
Italovini223 <italovinicius2018@gmail.com>
2022-10-21 02:46:12 -0300

    ♻️ refactor: change user_id origin

--- src/controllers/movieNotesControllers.js
+++ src/controllers/movieNotesControllers.js
@@ -5,7 +5,7 @@ class movieNotesControllers {
   
   async create(request, response){
     const {title, description, rating, tags} = request.body;
-    const {user_id} = request.params;
+    const user_id = request.user.id
 
     const note_id = await knex("movie_notes").insert({
       title,
@@ -42,7 +42,8 @@ class movieNotesControllers {
   } 
 
   async index(request, response){
-      const {user_id, title, tags} = request.query;
+      const {title, tags} = request.query;
+      const user_id = request.user.id
 
       let notes;
 
--- src/routes/movieNotes.routes.js
+++ src/routes/movieNotes.routes.js
@@ -1,13 +1,15 @@
 const {Router} = require("express");
+
 const movieNotesControllers = require("../controllers/movieNotesControllers");
+const ensureAuthenticated = require("../middleware/ensureAuthenticated");
 
 const notesRouter = Router();
 const notesController = new movieNotesControllers();
 
-notesRouter.get("/", notesController.index);
-notesRouter.post("/:user_id", notesController.create);
-notesRouter.get("/:id", notesController.show);
-notesRouter.delete("/:id", notesController.delete);
+notesRouter.get("/", ensureAuthenticated, notesController.index);
+notesRouter.post("/", ensureAuthenticated, notesController.create);
+notesRouter.get("/:id", ensureAuthenticated, notesController.show);
+notesRouter.delete("/:id", ensureAuthenticated, notesController.delete);
 
 
 module.exports = notesRouter;
