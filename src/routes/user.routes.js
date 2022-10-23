3fe79fadb32b1952b649ed8fc2a042b7d4865302
Italovini223 <italovinicius2018@gmail.com>
2022-10-23 12:13:33 -0300

    ✨ feat: add a route to load

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,14 +1,23 @@
 const {Router} = require("express");
+const multer = require("multer");
 
 const UserControllers = require("../controllers/userControllers");
 const ensureAuthenticated = require("../middleware/ensureAuthenticated");
+const uploadConfig = require("../configs/upload");
 
 const userRoutes = Router();
 
 const userControllers = new UserControllers();
+const upload = multer(uploadConfig.MULTER);
 
 userRoutes.post("/", userControllers.create);
 userRoutes.put("/", ensureAuthenticated,  userControllers.update);
+userRoutes.patch("/avatar", ensureAuthenticated, upload.single("avatar"), (req, res) => {
+  console.log(req.file.filename)
+
+  res.json()
+})
 userRoutes.delete("/", ensureAuthenticated,  userControllers.delete);
+
 
 module.exports = userRoutes;
