d8f17af25490809da013b7566a58af587f302e84
Italovini223 <italovinicius2018@gmail.com>
2022-10-23 13:00:23 -0300

    ✨ feat: add cors

--- src/server.js
+++ src/server.js
@@ -4,14 +4,17 @@ const database = require("./database/sqlite");
 const uploadConfig = require("./configs/upload")
 
 const express = require('express');
+const cors = require("cors");
+
 const app = express();
 
 const routes = require("./routes");
 
 const appError = require("./utils/appError")
 
-app.use(express.json())
-app.use(routes)
+app.use(cors());
+app.use(express.json());
+app.use(routes);
 app.use("/files", express.static(uploadConfig.UPLOADS_FOLDER));
 
 database();
