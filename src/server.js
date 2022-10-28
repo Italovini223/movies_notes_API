49d720ea35386b747ac064da9bf61b4d3d94c5b8
Italovini223 <italovinicius2018@gmail.com>
2022-10-28 19:39:41 -0300

    ✨ feat: add .env file

--- src/server.js
+++ src/server.js
@@ -1,3 +1,4 @@
+require("dotenv/config");
 require("express-async-errors");
 
 const database = require("./database/sqlite");
@@ -37,7 +38,7 @@ app.use((error, request, response, next) => {
 
 
 
-const PORT = 3333;
+const PORT = process.env.PORT || 3000;
 
 
 app.listen(PORT, () => {console.log(`Server is running on port ${PORT}`);});
